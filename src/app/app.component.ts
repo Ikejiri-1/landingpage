3b4698e56000e91af4cd841d13d2ad0eafea4abf
Ikejiri-1 <pedroike21@gmail.com>
2025-07-25 15:40:09 -0300

    criação da nav-bar e da section principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { PrincipalComponent } from './componente/principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CabecalhoComponent, PrincipalComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'landingpage';
