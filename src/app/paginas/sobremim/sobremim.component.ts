3f3977993a88a720315ca99395a935fb96322606
Ikejiri-1 <pedroike21@gmail.com>
2025-07-29 15:31:23 -0300

    ajustes

--- src/app/paginas/sobremim/sobremim.component.ts
+++ src/app/paginas/sobremim/sobremim.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../../componente/cabecalho/cabecalho.component';
+import { RodapeComponent } from '../../componente/rodape/rodape.component';
+import { SeparadorComponent } from '../../componente/separador/separador.component';
 
 @Component({
   selector: 'app-sobremim',
   standalone: true,
-  imports: [CabecalhoComponent],
+  imports: [CabecalhoComponent, RodapeComponent, SeparadorComponent],
   templateUrl: './sobremim.component.html',
   styleUrl: './sobremim.component.css',
 })
