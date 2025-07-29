7180be14b9515738f1e57715b57335344052a080
Ikejiri-1 <pedroike21@gmail.com>
2025-07-28 22:10:57 -0300

    rodapé criado

--- src/app/paginas/principal/principal.component.ts
+++ src/app/paginas/principal/principal.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../../componente/cabecalho/cabecalho.component';
 import { SeparadorComponent } from '../../componente/separador/separador.component';
 import { ConsultaComponent } from '../../componente/consulta/consulta.component';
+import { RodapeComponent } from '../../componente/rodape/rodape.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [CabecalhoComponent, SeparadorComponent, ConsultaComponent],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent,
+    SeparadorComponent,
+    ConsultaComponent,
+  ],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css',
 })
