557b54a6d26c039991b532f8ee3d5be3239878d1
Ikejiri-1 <pedroike21@gmail.com>
2025-07-29 17:35:18 -0300

    container e inicio do sobremim

--- src/app/paginas/sobremim/sobremim.component.ts
+++ src/app/paginas/sobremim/sobremim.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../../componente/cabecalho/cabecalho.component';
 import { RodapeComponent } from '../../componente/rodape/rodape.component';
 import { SeparadorComponent } from '../../componente/separador/separador.component';
+import { ContainerComponent } from '../../componente/container/container.component';
 
 @Component({
   selector: 'app-sobremim',
   standalone: true,
-  imports: [CabecalhoComponent, RodapeComponent, SeparadorComponent],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent,
+    SeparadorComponent,
+    ContainerComponent,
+  ],
   templateUrl: './sobremim.component.html',
   styleUrl: './sobremim.component.css',
 })
