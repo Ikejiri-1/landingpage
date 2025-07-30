b6f2ecb7d728152a1248b1bcb62fb71062c801da
Ikejiri-1 <pedroike21@gmail.com>
2025-07-29 23:48:03 -0300

    pagina de ozonioterapia

--- src/app/paginas/ozonioterapia/ozonioterapia.component.ts
+++ src/app/paginas/ozonioterapia/ozonioterapia.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../../componente/cabecalho/cabecalho.component';
+import { ContainerComponent } from '../../componente/container/container.component';
+import { SeparadorComponent } from '../../componente/separador/separador.component';
+import { RodapeComponent } from '../../componente/rodape/rodape.component';
 
 @Component({
   selector: 'app-ozonioterapia',
   standalone: true,
-  imports: [CabecalhoComponent],
+  imports: [
+    RodapeComponent,
+    CabecalhoComponent,
+    ContainerComponent,
+    SeparadorComponent,
+  ],
   templateUrl: './ozonioterapia.component.html',
   styleUrl: './ozonioterapia.component.css',
 })
