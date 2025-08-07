acb2ecf776f785e359e2d5489126e88e78b81028
Ikejiri-1 <pedroike21@gmail.com>
2025-08-06 23:26:15 -0300

    mudanças para icones e correções do css

--- src/app/componente/iconebotao/iconebotao.component.ts
+++ src/app/componente/iconebotao/iconebotao.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ConsultaComponent } from '../consulta/consulta.component';
 
 @Component({
   selector: 'app-iconebotao',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './iconebotao.component.html',
   styleUrl: './iconebotao.component.css',
 })
@@ -12,6 +14,10 @@ export class IconebotaoComponent {
     window.open('https://www.instagram.com/nataliadejesuss_/', '_blank');
   }
   redirecionarParaWhatsapp() {
-    window.open('https://wa.me/5512999999999', '_blank');
+    const message = 'Olá, gostaria de marcar uma consulta!';
+    const phone = '5512999999999';
+    const encodedMessage = encodeURIComponent(message.trim());
+    const cleanPhone = phone.replace(/\D/g, '');
+    window.open(`//wa.me/${cleanPhone}?text=${encodedMessage}`, '_blank');
   }
 }
