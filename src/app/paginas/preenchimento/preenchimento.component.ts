6f4c934cac8808eba1b87e20346c5f2b79baea51
Ikejiri-1 <pedroike21@gmail.com>
2025-08-07 17:28:42 -0300

    preenchimento facial feitoJ

--- src/app/paginas/preenchimento/preenchimento.component.ts
+++ src/app/paginas/preenchimento/preenchimento.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../../componente/cabecalho/cabecalho.component';
 import { IconebotaoComponent } from '../../componente/iconebotao/iconebotao.component';
+import { ContainerComponent } from '../../componente/container/container.component';
+import { RodapeComponent } from '../../componente/rodape/rodape.component';
 
 @Component({
   selector: 'app-preenchimento',
   standalone: true,
-  imports: [CabecalhoComponent, IconebotaoComponent],
+  imports: [
+    CabecalhoComponent,
+    IconebotaoComponent,
+    ContainerComponent,
+    RodapeComponent,
+  ],
   templateUrl: './preenchimento.component.html',
   styleUrl: './preenchimento.component.css',
 })
